8c33ec8fbe864e864b3782fc2966a2ff557f8ae9
Men-dez <69759418+Men-dez@users.noreply.github.com>
2021-09-29 17:09:44 -0500

    Add files via upload

---
+++ tallerTypeScript/main.ts
@@ -0,0 +1,47 @@
+import { Course } from "./course.js";
+import { dataCourses } from "./dataCourses.js";
+
+
+const coursesTbody: HTMLElement = document.getElementById('courses')!; // Nodo tbody que tiene el id="courses"
+const btnfilterByName: HTMLElement = document.getElementById('button-filterByName')!; // Nodo tbody que tiene el id="button-filterByName"
+const inputValue: HTMLInputElement = (document.getElementById("search-box") as HTMLInputElement);
+const textCreds: HTMLElement = document.getElementById('creds')!; // Nodo tbody que tiene el id="button-filterByName"
+const table: HTMLElement = document.getElementById('tableInfo')!; // Nodo tbody que tiene el id="button-filterByName"
+
+function renderCoursesInTable(courses: Course[]): void {
+  courses.forEach(c => {
+    let trElement = document.createElement("tr");
+    trElement.innerHTML = `<td>${c.name}</td>
+                           <td>${c.professor}</td>
+                           <td>${c.credits}</td>`;
+    coursesTbody.appendChild(trElement);
+  });
+}
+
+function getTotalCredits(courses: Course[]): number {
+    let totalCredits: number = 0;
+    courses.forEach((course) => totalCredits = totalCredits + course.credits);
+    return totalCredits;
+}
+
+function clearCoursesInTable(){
+  let bodyRef = table.getElementsByTagName('tbody')[0]; 
+  bodyRef.innerHTML = '';
+}
+
+function searchCourseByName(nameKey: string, courses: Course[]) {
+    return nameKey === '' ? dataCourses : courses.filter( c => 
+      c.name.match(nameKey));
+  } 
+
+function applyFilterByName() { 
+  let text = inputValue.value;
+  console.log(text)
+  text = (text == null) ? '' : text;
+  clearCoursesInTable();
+  let coursesFiltered: Course[] = searchCourseByName(text, dataCourses);
+  renderCoursesInTable(coursesFiltered);
+  textCreds.innerHTML = String(getTotalCredits(coursesFiltered));
+}
+  
+btnfilterByName.onclick = () => applyFilterByName();
---
+++ tallerTypeScript/scripts/main.js
@@ -0,0 +1,37 @@
+import { dataCourses } from "./dataCourses.js";
+var coursesTbody = document.getElementById('courses'); // Nodo tbody que tiene el id="courses"
+var btnfilterByName = document.getElementById('button-filterByName'); // Nodo tbody que tiene el id="button-filterByName"
+var inputValue = document.getElementById("search-box");
+var textCreds = document.getElementById('creds'); // Nodo tbody que tiene el id="button-filterByName"
+var table = document.getElementById('tableInfo'); // Nodo tbody que tiene el id="button-filterByName"
+function renderCoursesInTable(courses) {
+    courses.forEach(function (c) {
+        var trElement = document.createElement("tr");
+        trElement.innerHTML = "<td>" + c.name + "</td>\n                           <td>" + c.professor + "</td>\n                           <td>" + c.credits + "</td>";
+        coursesTbody.appendChild(trElement);
+    });
+}
+function getTotalCredits(courses) {
+    var totalCredits = 0;
+    courses.forEach(function (course) { return totalCredits = totalCredits + course.credits; });
+    return totalCredits;
+}
+function clearCoursesInTable() {
+    var bodyRef = table.getElementsByTagName('tbody')[0];
+    bodyRef.innerHTML = '';
+}
+function searchCourseByName(nameKey, courses) {
+    return nameKey === '' ? dataCourses : courses.filter(function (c) {
+        return c.name.match(nameKey);
+    });
+}
+function applyFilterByName() {
+    var text = inputValue.value;
+    console.log(text);
+    text = (text == null) ? '' : text;
+    clearCoursesInTable();
+    var coursesFiltered = searchCourseByName(text, dataCourses);
+    renderCoursesInTable(coursesFiltered);
+    textCreds.innerHTML = String(getTotalCredits(coursesFiltered));
+}
+btnfilterByName.onclick = function () { return applyFilterByName(); };
